48b32209ae60ca4e4d96c224a87745d6618c9821
Mike <mikeadkison@gmail.com>
2016-08-11 17:00:55 -0400

    update comments about Cutscene

--- core/src/com/mygdx/game/server/model/Cutscene.java
+++ core/src/com/mygdx/game/server/model/Cutscene.java
@@ -2,12 +2,15 @@ package com.mygdx.game.server.model;
 
 /**
  * Represents a cutscene which is acted out incrementally each tick.
- * A cutscene is read from a file describing the cutscene.
  * When a cutscene starts, the GameState is set to CUTSCENE.
  * When a cutscene ends, the GameState is set to GAME
+ * Each individual cutscene is a subclass extending this class.
+ * The behavior of each cutscene will be specified in its subclass.
  * @author elimonent
  *
  */
 public abstract class Cutscene implements Actable {
-
+	protected void end() {
+		//set the global GameState back to GAME here.
+	}
 }
--- core/src/com/mygdx/game/server/model/Server.java
+++ core/src/com/mygdx/game/server/model/Server.java
@@ -4,8 +4,18 @@ package com.mygdx.game.server.model;
  * 
  * @author elimonent
  *
- * A singleton which keeps track of the server's state.
+ * A singleton which keeps track of the server's state
+ *  and simulates the world (ticks)
  */
-public class Server {
+public class Server implements Runnable {
+
+	/**
+	 * The server loop
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
