9e627b3ae175736357ecfdba0e5ef7c55924063f
Mike <mikeadkison@gmail.com>
2016-08-12 16:34:01 -0400

    add comment to Server

--- core/src/com/mygdx/game/server/model/Server.java
+++ core/src/com/mygdx/game/server/model/Server.java
@@ -6,6 +6,8 @@ package com.mygdx.game.server.model;
  *
  * A singleton which keeps track of the server's state
  *  and simulates the world (ticks)
+ *  
+ * A server instance is created by a client using a button in the client's menu.
  */
 public class Server implements Runnable {
 
