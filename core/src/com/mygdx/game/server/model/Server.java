8ae0848419b816fe0fc013c8a7e510a591d0ff74
Mike <mikeadkison@gmail.com>
2016-08-16 07:05:15 -0400

    add some fields and a method to the Server body

--- core/src/com/mygdx/game/server/model/Server.java
+++ core/src/com/mygdx/game/server/model/Server.java
@@ -1,5 +1,11 @@
 package com.mygdx.game.server.model;
 
+import com.mygdx.game.server.controller.ServerCommunicator;
+import com.mygdx.game.server.model.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * @author elimonent
@@ -8,8 +14,17 @@ package com.mygdx.game.server.model;
  *  and simulates the world (ticks)
  *  
  * A server instance is created by a client using a button in the client's menu.
+ * The player who creates a server instance with their client is the Host.
  */
 public class Server implements Runnable {
+
+	private final List<Player> players = new ArrayList<Player>();
+	private GameMap map; // The current game map.
+	private final ServerCommunicator communicator = new ServerCommunicator();
+	private final float TICKRATE = .05f; //How often the server updates the game world.
+	private GameState state;
+	private Cutscene currCutscene;
+
 
 	/**
 	 * The server loop
@@ -18,6 +33,11 @@ public class Server implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+
+	public void setState(GameState state) {
+		this.state = state;
 	}
 
 }
