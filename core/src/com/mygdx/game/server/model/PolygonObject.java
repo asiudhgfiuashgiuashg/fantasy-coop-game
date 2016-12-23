c758c88e63b051ef4c9d19f97d1b272dfe6d3030
Sawyer Harris <sawyerh99@yahoo.com>
2016-12-23 15:40:08 -0500

    Added boundary loading in server MapLoader and abstracted polygon loading method in TilePolygonLoader

--- core/src/com/mygdx/game/server/model/PolygonObject.java
+++ core/src/com/mygdx/game/server/model/PolygonObject.java
@@ -5,12 +5,18 @@ import com.mygdx.game.shared.util.CollideablePolygon;
 
 /**
  * An object which has a collideablepolygon as one of its fields.
+ * 
  * @author elimonent
+ * @author Sawyer Harris
  *
  */
 public abstract class PolygonObject {
 	protected CollideablePolygon polygon;
-	
+
+	protected PolygonObject(CollideablePolygon polygon) {
+		this.polygon = polygon;
+	}
+
 	protected boolean collides(PolygonObject other) {
 		return other.polygon.collides(this.polygon, new Intersector.MinimumTranslationVector());
 	}
