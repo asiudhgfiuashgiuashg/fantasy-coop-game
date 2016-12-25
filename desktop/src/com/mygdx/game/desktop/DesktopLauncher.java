15003b22fda9f6d351c51f2fdb7bfd089e1ff4dc
AEthura <arthurofavalon@gmail.com>
2016-12-24 19:26:16 -0500

    Experimenting with screens and orthographiccamera

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.mygdx.game.desktop;
 
+import testing.Test;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.client.model.GameClient;
