f298e4d53acfdbeb15a504544dfca70ad28707b4
mikeadkison <mikeadkison@gmail.com>
2017-04-03 03:52:45 -0400

    add tree asset for prototype

--- core/src/com/mygdx/game/server/model/entity/enemy/TestEnemy2.java
+++ core/src/com/mygdx/game/server/model/entity/enemy/TestEnemy2.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.server.model.entity.enemy;
 
 import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.server.model.GameServer;
 import com.mygdx.game.server.model.PolygonObject;
 import com.mygdx.game.shared.model.CollideablePolygon;
 
