e42c5f84a212da7d633fd598cf780687acc3751f
bshah11 <bshah11@uncc.edu>
2017-04-09 03:42:34 -0400

    player moves properly now, with no acceleration or deceleration. Movement is currently not synced between clients.

--- core/src/com/mygdx/game/client/controller/KeyboardProcessor.java
+++ core/src/com/mygdx/game/client/controller/KeyboardProcessor.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.InputAdapter;
 import com.mygdx.game.client.model.GameClient;
 import com.mygdx.game.client.model.keybinds.Keybinds;
 import com.mygdx.game.client.model.keybinds.Keybinds.Input;
+import com.mygdx.game.client.model.entity.player.Player;
 
 public class KeyboardProcessor extends InputAdapter {	
 	private Keybinds binds;
+	private Player localPlayer;
 	
 	public KeyboardProcessor(Keybinds binds) {
 		this.binds = binds;
@@ -16,17 +18,17 @@ public class KeyboardProcessor extends InputAdapter {
 	public boolean keyDown(int keycode) {
 		Keybinds.Input input = binds.get(keycode);
 		System.out.println(input);
+		localPlayer = GameClient.getInstance().getMap().localPlayer;
 
 		if (input == Input.UP) {
-			GameClient.getInstance().getMap().localPlayer.moveUp();
+			localPlayer.up = true;
 		} else if (input == Input.DOWN) {
-			GameClient.getInstance().getMap().localPlayer.moveDown();
+			localPlayer.down = true;
 		} else if (input == Input.LEFT) {
-			GameClient.getInstance().getMap().localPlayer.moveLeft();
+			localPlayer.left = true;
 		} else if (input == Input.RIGHT) {
-			GameClient.getInstance().getMap().localPlayer.moveRight();
+			localPlayer.right = true;
 		}
-
 		/* 
 		 * EXAMPLE of what happens next
 		 * 
@@ -41,17 +43,19 @@ public class KeyboardProcessor extends InputAdapter {
 	
 	@Override
 	public boolean keyUp(int keycode) {
+		 localPlayer = GameClient.getInstance().getMap().localPlayer;
+		 
 		Keybinds.Input input = binds.get(keycode);
 		if (input == Input.UP) {
-			GameClient.getInstance().getMap().localPlayer.moveUp();
+			localPlayer.up = false;
 		} else if (input == Input.DOWN) {
-			GameClient.getInstance().getMap().localPlayer.moveDown();
+			localPlayer.down = false;
 		} else if (input == Input.LEFT) {
-			GameClient.getInstance().getMap().localPlayer.moveLeft();
+			localPlayer.left = false;
 		} else if (input == Input.RIGHT) {
-			GameClient.getInstance().getMap().localPlayer.moveRight();
+			localPlayer.right = false;
 		}
-
+		
 		return false;
 	}
 }
--- core/src/com/mygdx/game/client/model/entity/player/Player.java
+++ core/src/com/mygdx/game/client/model/entity/player/Player.java
@@ -10,30 +10,33 @@ import com.mygdx.game.client.model.entity.DynamicEntity;
 public abstract class Player extends DynamicEntity {
 
 	private static final float movementForce = 1000; // how much force to apply when moving
-	private Vector2 upMovementForce = new Vector2(0, movementForce);
-	private Vector2 downMovementForce = new Vector2(0, -movementForce);
-	private Vector2 leftMovementForce = new Vector2(-movementForce, 0);
-	private Vector2 rightMovementForce = new Vector2(movementForce, 0);
+	private float speed = 20;
+	public boolean up = false;
+	public boolean down = false;
+	public boolean right = false;
+	public boolean left = false;
 
 	public Player(String entUid, String className, Vector2 pos, int visLayer) {
 		super(entUid, className, pos, visLayer);
 		setMass(1);
 	}
-
-	public void moveUp() {
-		applyForce(upMovementForce);
-	}
-
-	public void moveDown() {
-		applyForce(downMovementForce);
-	}
-
-	public void moveLeft() {
-		applyForce(leftMovementForce);
-	}
-
-	public void moveRight() {
-		applyForce(rightMovementForce);
-
+	
+	@Override
+	public void tick(float deltaT) {
+		super.tick(deltaT);
+		if (up && !down) {
+			setVelocity(new Vector2(getVelocity().x, speed));
+		} else if (!up && down) {
+			setVelocity(new Vector2(getVelocity().x, -speed));
+		} else {
+			setVelocity(new Vector2(getVelocity().x, 0));
+		}
+		if (right && !left) {
+			setVelocity(new Vector2(speed, getVelocity().y));
+		} else if (!right && left) {
+			setVelocity(new Vector2(-speed, getVelocity().y));
+		} else {
+			setVelocity(new Vector2(0, getVelocity().y));
+		}
 	}
 }
